d62ae2184e3a366abd860898e2baf43fbad4d2c3
Pyae Phyo Aung <learncloudhub@gmail.com>
2026-09-14 13:51:49 +0630

    result.py added

---
+++ result.py
@@ -0,0 +1,6 @@
+name = input("Type Student Name: ")
+sub1 = int(input("Myanmar exam mark: "))
+sub2 = int(input("English exam mark: "))  
+average = (sub1 + sub2) // 2
+print(f"Name: {name}")
+print(f"Average mark: {average}")
