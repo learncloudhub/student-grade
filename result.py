0c8c00a819a448794b53c6810b0db75bcfc87190
Pyae Phyo Aung <learncloudhub@gmail.com>
2026-09-14 14:12:48 +0630

    Updated

--- result.py
+++ result.py
@@ -3,4 +3,13 @@ sub1 = int(input("Myanmar exam mark: "))
 sub2 = int(input("English exam mark: "))  
 average = (sub1 + sub2) // 2
 print(f"Name: {name}")
+
+if sub1 > 40 and sub2 > 40:
+    result = "Passed"
+else:
+    result = "Failed"
+
+print(f"Myanmar, {sub1}, {result}")
+print(f"English, {sub2}, {result}")
+
 print(f"Average mark: {average}")
